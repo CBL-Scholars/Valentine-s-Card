e4eded9191ad024446249b7e86a854f541918e21
CBT-Scholars <124578159+CBT-Scholars@users.noreply.github.com>
2023-02-06 15:20:43 +0200

    Delete Line directory

--- Line/script.js
+++
@@ -1,9 +0,0 @@
-
-function function1() {
- document.getElementById("firstpage").style.display="none";
- document.getElementById("secondpage").style.display="block";
-}
-function function2() {
- document.getElementById("firstpage").style.display="block";
- document.getElementById("secondpage").style.display="none";
-}
